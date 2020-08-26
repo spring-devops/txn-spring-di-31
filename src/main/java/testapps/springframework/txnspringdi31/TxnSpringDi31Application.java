ecdb5ec3e7bd5e495ee558dbc42b7f2ecd390733
Spring DevOps <harman009-springdevops@gmail.command>
2020-08-26 10:11:21 -0400

    Added Controller

--- src/main/java/testapps/springframework/txnspringdi31/TxnSpringDi31Application.java
+++ src/main/java/testapps/springframework/txnspringdi31/TxnSpringDi31Application.java
@@ -2,12 +2,21 @@ package testapps.springframework.txnspringdi31;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import testapps.springframework.txnspringdi31.controllers.MyController;
 
 @SpringBootApplication
 public class TxnSpringDi31Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TxnSpringDi31Application.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(TxnSpringDi31Application.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController") ;
+
+		String greeting = myController.sayHello();
+
+		System.out.println("greeting");
 	}
 
 }
---
+++ src/main/java/testapps/springframework/txnspringdi31/controllers/MyController.java
@@ -0,0 +1,11 @@
+package testapps.springframework.txnspringdi31.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello() {
+        System.out.println(">>>>>>>>>>>>>> Hello World!");
+        return "Hi Folks!";
+    }
+}
