21ca04bd7b47f942af9e8d5ac0d81c259aeea40c
Spring DevOps <harman009-springdevops@gmail.command>
2020-08-26 13:59:05 -0400

    Spring DI Attempt on Property Fail To Start

--- src/main/java/testapps/springframework/txnspringdi31/controllers/PropertyInjectedController.java
+++ src/main/java/testapps/springframework/txnspringdi31/controllers/PropertyInjectedController.java
@@ -1,10 +1,12 @@
 package testapps.springframework.txnspringdi31.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import testapps.springframework.txnspringdi31.services.GreetingService;
 
 @Controller
 public class PropertyInjectedController {
+    @Autowired
     GreetingService greetingService;
 
     public String getGreeting() {
