caec7550ce9247eb4f4c68ec079bee28ca993ecd
Spring DevOps <harman009-springdevops@gmail.command>
2020-08-26 15:44:23 -0400

    Spring DI Profiles Fixed with Properties

--- src/main/java/testapps/springframework/txnspringdi31/services/I18nEnglishGreetingService.java
+++ src/main/java/testapps/springframework/txnspringdi31/services/I18nEnglishGreetingService.java
@@ -3,7 +3,7 @@ package testapps.springframework.txnspringdi31.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile("EN")
 @Service("I18nService")
 public class I18nEnglishGreetingService implements GreetingService{
     @Override
