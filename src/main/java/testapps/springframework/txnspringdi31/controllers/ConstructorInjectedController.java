e4e4770df6924962368ddce0d9e1ab7692bc5e44
Spring DevOps <harman009-springdevops@gmail.command>
2020-08-26 14:13:22 -0400

    Spring DI Attempt on Constructor Fixed

--- src/main/java/testapps/springframework/txnspringdi31/controllers/ConstructorInjectedController.java
+++ src/main/java/testapps/springframework/txnspringdi31/controllers/ConstructorInjectedController.java
@@ -1,15 +1,18 @@
 package testapps.springframework.txnspringdi31.controllers;
 
+import org.springframework.stereotype.Controller;
 import testapps.springframework.txnspringdi31.services.GreetingService;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
+    //Constructor based AUTOWIRED is not needed since Spring 4.2 It is OPTIONAL now, never needed. If present, it does not do anything extra
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
     public String getGreeting() {
-        return "ConstructorInjectedController" + greetingService.sayGreeting();
+        return "ConstructorInjectedController >>>> " + greetingService.sayGreeting();
     }
 }
